79b6cdf9307782c72291b263293b07d7e202a996
Ron Korstanje <ron.korstanje@jax.org>
2016-12-19 18:12:59 -0500

    added data manipulation

--- pee-H-calculator.R
+++ pee-H-calculator.R
@@ -3,3 +3,8 @@ library(data.table)
 data <- fread("pH_results.csv", select =c("Mean"))
 
 ## reformat the table to 3 values (R, G, and B) per row
+data_list<-list(data)
+pH_data<-matrix(unlist(data_list), ncol=3, byrow=TRUE)
+
+## create matrix with the data from the 6 buffers
+buffer_data<-head(pH_data,6)
