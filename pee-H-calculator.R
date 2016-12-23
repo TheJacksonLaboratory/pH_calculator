ee476e7c9ad92581dbc3683846c0f75b4f952264
Ron Korstanje <ron.korstanje@jax.org>
2016-12-23 16:27:46 -0500

    Finished first complete version of the pee H calculator

--- pee-H-calculator.R
+++ pee-H-calculator.R
@@ -8,3 +8,21 @@ pH_data<-matrix(unlist(data_list), ncol=3, byrow=TRUE)
 
 ## create matrix with the data from the 6 buffers
 buffer_data<-head(pH_data,6)
+buffers=matrix(c(4,4.63,5,6,6.8,7),nrow=6,ncol=1)
+tbuffers<-cbind2(buffers,buffer_data)
+colnames(tbuffers)<- c("pH","R","G","B")
+tbuffersframe<-as.data.frame(tbuffers)
+
+## multiple regression on tbuffers
+fit<-lm(pH ~ R + G + B, data=tbuffersframe)
+intercept<-summary(fit)$coefficients[1,1]
+Rest<-summary(fit)$coefficient[2,1]
+Gest<-summary(fit)$coefficient[3,1]
+Best<-summary(fit)$coefficient[4,1]
+
+## calculation of pH in matrix
+pH_results<-matrix(apply(pH_data, 1, function(x) intercept+x[1]*Rest+x[2]*Gest+x[3]*Best))
+colnames(pH_results)<- c("sample","pH")
+
+## save results to a file
+write.csv(pH_results, file="pH_results.csv")
